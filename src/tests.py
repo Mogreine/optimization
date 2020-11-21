0b77e0e14a977f67d023899db989f5eb3f75321f
Mogreine <north7311@gmail.com>
2020-11-21 14:44:19 +0300

    Plotting

--- src/tests.py
+++ src/tests.py
@@ -1,35 +1,86 @@
 import numpy as np
 from scipy.special import expit
-from src.hw2.oracle import make_oracle, OracleTester
+from src.hw2.oracle import make_oracle
 from src.hw2.optimization import gradient_descent, newton, newton_hess_free
 from src.hw2.oracle import Oracle
 from sklearn.datasets import load_svmlight_file
 from sklearn.linear_model import LogisticRegression
 from scipy.sparse import hstack
 import timeit
+import numpy as np
+import plotly.graph_objs as go
+import plotly
 
 
 a = np.array([-1000000000000, 2, 3])
 b = np.array([1, 1, 1])
 
 
-def test_optimization():
-    oracle = make_oracle('hw2/data/a1a.txt')
+def _plot(data, graph_name):
+    names_map = {
+        'time': 0,
+        'calls': 1,
+        'iters': 2
+    }
+    ind = names_map[graph_name]
+    traces = []
+    colors = {
+        'gs': {'color': 'rgba(255, 0, 0, 0.8)'},
+        'brent_scipy': {'color': 'rgba(0, 255, 0, 0.8)'},
+        'armijo': {'color': 'rgba(0, 0, 255, 0.8)'},
+        'wolfe': {'color': 'rgba(255, 0, 255, 0.8)'},
+        'lip': {'color': 'rgba(0, 0, 0, 0.8)'}
+    }
+    for method_name, method_data in data.items():
+        w_opt, rk_arr, elapsed_time_arr, oracle_calls_arr, iters_arr = method_data
 
-    max_iter = int(1e4)
+        traces.append(
+            go.Scatter(
+                x=method_data[ind + 2],
+                y=method_data[1],
+                mode='lines+markers',
+                marker=colors[method_name],
+                name=method_name
+                # xaxis='x1',
+                # yaxis='y1'
+            )
+        )
 
-    its = []
-    for i in range(5):
+    layout = go.Layout(
+        xaxis=dict(title=graph_name, zeroline=False),
+        yaxis=dict(title='$log_{10}|F(w_i) - F(w^*)|$', zeroline=False)
+    )
+
+    fig = go.Figure(data=traces, layout=layout)
+    plotly.offline.iplot(fig)
+
+
+def plot(data: dict, graph=('time', 'calls', 'iters')):
+    for graph_name in graph:
+        _plot(data, graph_name)
+
+
+def run_tests(oracle, w_opt, line_search_methods=None, max_iter=10000, n_tests=1):
+    line_search_methods = ['gs', 'brent_scipy', 'armijo', 'wolfe'] if line_search_methods is None else line_search_methods
+    time = []
+    rk = []
+    res = {}
+    for method in line_search_methods:
         w_0 = np.random.normal(0, 1, oracle.features)
-        w_0 = np.zeros(oracle.features)
-        w_0 = np.random.uniform(-1 / np.sqrt(oracle.features), 1 / np.sqrt(oracle.features), size=oracle.features)
+        # w_0 = np.zeros(oracle.features)
+        # w_0 = np.random.uniform(-1 / np.sqrt(oracle.features), 1 / np.sqrt(oracle.features), size=oracle.features)
         # w_0 = np.ones(oracle.features)
-        w_opt, iters = newton(oracle, w_0, line_search_method='brent', max_iter=max_iter, tol=1e-8)
-        its.append(iters)
+        w_opt, rk_arr, elapsed_time_arr, oracle_calls_arr, iters_arr = gradient_descent(oracle,
+                                                                                        w_0,
+                                                                                        line_search_method=method,
+                                                                                        max_iter=max_iter,
+                                                                                        tol=1e-8)
+        print(f'{method}: {iters_arr[-1]}, {oracle.value(w_opt)}')
+        res[method] = (w_opt, np.log10(np.abs(rk_arr - oracle.value(w_opt))), elapsed_time_arr, oracle_calls_arr, iters_arr)
+    return res
 
 
-    print(f'Av. iters: {np.mean(its)}')
-
+def get_w_true():
     X, y = load_svmlight_file('hw2/data/a1a.txt')
     ones_col = np.ones((X.shape[0], 1))
     X = hstack((X, ones_col))
@@ -39,16 +90,20 @@ def test_optimization():
 
     clf = LogisticRegression(penalty='none',
                              tol=1e-8,
-                             max_iter=max_iter,
+                             max_iter=10000,
                              random_state=1,
-                             fit_intercept=False,
-                             solver='newton-cg'
+                             fit_intercept=False
                              )
     clf.fit(X, y)
     w_sk = clf.coef_[0]
-    # print(w_sk)
-    print(f"mine   : {oracle.value(w_opt)}")
-    print(f"sklearn: {oracle.value(w_sk)}")
+    return w_sk
+
+
+def test_optimization():
+    w_opt = get_w_true()
+    oracle = make_oracle('hw2/data/a1a.txt')
+    res = run_tests(oracle, w_opt, line_search_methods=None, max_iter=10000)
+    plot(res)
 
 
 def test_shit():
